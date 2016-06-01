cebda0ef07e446b406638b0ab698c17cdfc4651e
austinchen <dong.chen@sysongy.com>
2016-06-01 19:23:46 +0800

    增加新的controller给客户端提供服务

--- src/main/java/com/sysongy/poms/base/controller/CRMBaseContoller.java
+++ src/main/java/com/sysongy/poms/base/controller/CRMBaseContoller.java
@@ -1,10 +1,9 @@
 package com.sysongy.poms.base.controller;
 
-import java.util.Properties;
-
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-
+import com.sysongy.poms.base.model.AjaxJson;
+import com.sysongy.poms.base.model.InterfaceConstants;
+import com.sysongy.util.GlobalConstant;
+import com.sysongy.util.PropertyUtil;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
@@ -15,8 +14,9 @@ import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.SessionAttributes;
 import org.springframework.web.bind.support.SessionStatus;
 
-import com.sysongy.util.GlobalConstant;
-import com.sysongy.util.PropertyUtil;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.util.Properties;
 
 /**
  * @FileName     :  BaseContoller.java
@@ -31,7 +31,8 @@ import com.sysongy.util.PropertyUtil;
  */
 @Controller
 @SessionAttributes({"currUser","systemId","userId","menuCode","menuIndex"})
-public class BaseContoller {
+@RequestMapping("/crmService")
+public class CRMBaseContoller {
 	
 	protected Logger logger = LoggerFactory.getLogger(this.getClass());
 
@@ -64,17 +65,19 @@ public class BaseContoller {
 
         return "common/g_main";
     }
-    @RequestMapping(value = {"/web/login"})  
-    public String login( HttpServletRequest request,HttpServletResponse response,ModelMap map){ 
-    	String userName = request.getParameter("userName");
-    	String password = request.getParameter("password");
-    	String returnPath = "login";
-    	/*if(userName != null && password != null && userName.equals("wdq") && password.equals("wdq123456")){*/
-    		map.addAttribute("current_module", "webpage/demo/demo");
-    		returnPath = "common/g_main";
-    	/*}*/
-
-    	return returnPath;
+    @RequestMapping(value = {"/web/login"})
+    @ResponseBody
+    public AjaxJson login( HttpServletRequest request,HttpServletResponse response,ModelMap map){
+        AjaxJson ajaxJson = new AjaxJson();
+        String userName = request.getParameter("userName");
+        String password = request.getParameter("password");
+        if(userName.equalsIgnoreCase("test")){
+            return ajaxJson;
+        } else {
+            ajaxJson.setSuccess(false);
+            ajaxJson.setMsg(InterfaceConstants.ERROR_AUTHORITY);
+        }
+    	return ajaxJson;
     }
 
     /**
@@ -89,32 +92,6 @@ public class BaseContoller {
     	sessionStatus.setComplete();
     	return "redirect:/";
     }
-    
-    /**
-     * 跳转控制面板
-     * @param request
-     * @param response
-     * @param map
-     * @return
-     */
-    @RequestMapping(value = {"/web/panel/list"})  
-    public String panelList( HttpServletRequest request,HttpServletResponse response,ModelMap map ){  
-    	
-    	return "/demo/panel_list";
-    }
-    
-    /**
-     * 跳转控制面板
-     * @param map
-     * @return
-     */
-    @RequestMapping(value = {"/web/menu/update"})  
-    @ResponseBody
-    public String updateMenu( @RequestParam String menuCode, @RequestParam String menuIndex,ModelMap map ){  
-    	String result = "suc";
-    	map.put("menuCode", menuCode);
-    	map.put("menuIndex", menuIndex);
-    	return result;
-    }
+
     
 }
---
+++ src/main/java/com/sysongy/poms/base/model/AjaxJson.java
@@ -0,0 +1,56 @@
+package com.sysongy.poms.base.model;
+
+import net.sf.json.JSONObject;
+
+import java.util.Map;
+
+/**
+ * Created by Administrator on 2016/6/1.
+ */
+public class AjaxJson {
+
+    private boolean success = true;// 是否成功
+    private String msg = "操作成功";// 提示信息
+    private Object obj = null;// 其他信息
+    private Map<String, Object> attributes;// 其他参数
+    public Map<String, Object> getAttributes() {
+        return attributes;
+    }
+
+    public void setAttributes(Map<String, Object> attributes) {
+        this.attributes = attributes;
+    }
+
+    public String getMsg() {
+        return msg;
+    }
+
+    public void setMsg(String msg) {
+        this.msg = msg;
+    }
+
+    public Object getObj() {
+        return obj;
+    }
+
+    public void setObj(Object obj) {
+        this.obj = obj;
+    }
+
+    public boolean isSuccess() {
+        return success;
+    }
+
+    public void setSuccess(boolean success) {
+        this.success = success;
+    }
+
+    public String getJsonStr(){
+        JSONObject obj = new JSONObject();
+        obj.put("success", this.isSuccess());
+        obj.put("msg", this.getMsg());
+        obj.put("obj", this.obj);
+        obj.put("attributes", this.attributes);
+        return obj.toString();
+    }
+}
