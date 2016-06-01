1dc1569185e51caaef97d76367f642918e6d46b1
wdq <dongqiang.wang@126.com>
2016-06-01 19:38:09 +0800

    修改数据库连接

--- src/main/java/com/sysongy/poms/base/controller/BaseContoller.java
+++ src/main/java/com/sysongy/poms/base/controller/BaseContoller.java
@@ -5,12 +5,8 @@ import java.util.Properties;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-import com.sysongy.poms.base.model.CurrUser;
-import com.sysongy.poms.permi.model.SysUser;
-import com.sysongy.poms.permi.service.SysUserService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -34,22 +30,16 @@ import com.sysongy.util.PropertyUtil;
  *
  */
 @Controller
-@SessionAttributes({"currUser","userId"})
+@SessionAttributes({"currUser","systemId","userId","menuCode","menuIndex"})
 public class BaseContoller {
 	
 	protected Logger logger = LoggerFactory.getLogger(this.getClass());
+
 	private static final long serialVersionUID = 6357869213649815390L;
+	
 	public Properties prop = PropertyUtil.read(GlobalConstant.CONF_PATH);
 
-    @Autowired
-    SysUserService sysUserService;
-
-    /**
-     * 进入登录页面
-     * @param map
-     * @return
-     */
-    @RequestMapping(value = {"","/"})
+    @RequestMapping(value = {"","/","/test"})  
     public String index(ModelMap map){  
     	/*ModelAndView result = new ModelAndView();
     	String pmcName = "登录测试成功！";
@@ -57,33 +47,32 @@ public class BaseContoller {
     	result.addObject("current_module", "webpage/test");
         result.setViewName("comm/g_main");*/
         
+        map.addAttribute("current_module", "webpage/demo/panel");
+        
         return "login";
     }
 
-    /**
-     * 用户登录
-     * @param request
-     * @param response
-     * @param map
-     * @return
-     */
+    @RequestMapping(value = {"main"})
+    public String main(ModelMap map){
+    	/*ModelAndView result = new ModelAndView();
+    	String pmcName = "登录测试成功！";
+    	result.addObject("pmcName", pmcName);
+    	result.addObject("current_module", "webpage/test");
+        result.setViewName("comm/g_main");*/
+
+        map.addAttribute("current_module", "webpage/demo/panel");
+
+        return "common/g_main";
+    }
     @RequestMapping(value = {"/web/login"})  
     public String login( HttpServletRequest request,HttpServletResponse response,ModelMap map){ 
     	String userName = request.getParameter("userName");
     	String password = request.getParameter("password");
     	String returnPath = "login";
-        SysUser sysUser = new SysUser();
-        sysUser.setUserName(userName);
-        sysUser.setPassword(password);
-
-        sysUser = sysUserService.queryUserByAccount(sysUser);
-    	if(sysUser != null && userName != null && password != null){
-            CurrUser currUser = new CurrUser();
-            currUser.setUserId(sysUser.getSysUserId());
-            map.addAttribute("currUser",currUser);
-
-            returnPath = "common/g_main";
-    	}
+    	/*if(userName != null && password != null && userName.equals("wdq") && password.equals("wdq123456")){*/
+    		map.addAttribute("current_module", "webpage/demo/demo");
+    		returnPath = "common/g_main";
+    	/*}*/
 
     	return returnPath;
     }
@@ -110,9 +99,9 @@ public class BaseContoller {
      */
     @RequestMapping(value = {"/web/panel/list"})
     @ResponseBody
-    public String panelList( HttpServletRequest request,HttpServletResponse response,ModelMap map ){  
-    	
-    	return "爱上当姐夫拉克时间的弗拉斯柯达";
+    public String panelList( HttpServletRequest request,HttpServletResponse response,ModelMap map ){
+
+        return "login";
     }
     
     /**
