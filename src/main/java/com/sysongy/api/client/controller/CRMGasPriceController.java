0e3f5b341a2dc43d0d20863ea9d717747bd4fd4e
austinchen <dong.chen@sysongy.com>
2016-06-14 18:32:32 +0800

    增加气品管理

---
+++ src/main/java/com/sysongy/api/client/controller/CRMGasPriceController.java
@@ -0,0 +1,16 @@
+package com.sysongy.api.client.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ * Created by Administrator on 2016/6/14.
+ */
+
+@Controller
+@RequestMapping("/crmGasPriceService")
+public class CRMGasPriceController {
+
+    
+
+}
