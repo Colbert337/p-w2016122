0626ccbabad3ee4c72e3d89f7c8b82360fc7245d
wdq <dongqiang.wang@126.com>
2016-06-21 19:02:06 +0800

    添加车辆管理相关代码

--- src/main/java/com/sysongy/tcms/advance/controller/TcVehicleController.java
+++ src/main/java/com/sysongy/tcms/advance/controller/TcVehicleController.java
@@ -1,7 +1,18 @@
 package com.sysongy.tcms.advance.controller;
 
+import com.github.pagehelper.PageInfo;
 import com.sysongy.poms.base.controller.BaseContoller;
+import com.sysongy.poms.base.model.CurrUser;
+import com.sysongy.poms.driver.model.SysDriver;
+import com.sysongy.poms.driver.service.DriverService;
+import com.sysongy.util.Encoder;
+import com.sysongy.util.GlobalConstant;
+import com.sysongy.util.RedisClientInterface;
+import com.sysongy.util.UUIDGenerator;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 /**
@@ -17,5 +28,74 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 @RequestMapping("/web/tcms/vehicle")
 public class TcVehicleController extends BaseContoller {
+
+
+    @Autowired
+    DriverService driverService;
+    @Autowired
+    RedisClientInterface redisClientImpl;
+
+    /**
+     * 查询车辆列表
+     * @return
+     */
+    @RequestMapping("/list/page")
+    public String queryVehicleListPage(@ModelAttribute CurrUser currUser, SysDriver driver, ModelMap map){
+        String stationId = currUser.getStationId();
+        if(driver.getPageNum() == null){
+            driver.setPageNum(GlobalConstant.PAGE_NUM);
+            driver.setPageSize(GlobalConstant.PAGE_SIZE);
+        }
+        driver.setStationId(stationId);
+
+        //封装分页参数，用于查询分页内容
+        PageInfo<SysDriver> driverPageInfo = new PageInfo<SysDriver>();
+        try {
+            driverPageInfo = driverService.querySearchDriverList(driver);
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+
+        map.addAttribute("driverList",driverPageInfo.getList());
+        map.addAttribute("pageInfo",driverPageInfo);
+        map.addAttribute("driver",driver);
+
+        return "webpage/tcms/driver/driver_list";
+    }
+
+
+    /**
+     * 添加车辆
+     * @param currUser 当前用户
+     * @param driver 司机
+     * @param map
+     * @return
+     */
+    @RequestMapping("/save")
+    public String saveDriver(@ModelAttribute("currUser") CurrUser currUser, SysDriver driver, ModelMap map){
+        int userType = currUser.getUser().getUserType();
+        int result = 0;
+
+        String stationId = currUser.getStationId();
+        String operation = "insert";
+        String payCode = driver.getPayCode();
+        String verificationCode = driver.getUserName();
+        driver.setUserName(null);
+        driver.setUserStatus("0");//0 使用中 1 已冻结
+        driver.setChecked_status("0");//审核状态 0 新注册 1 待审核 2 已通过 3 未通过
+        driver.setCheckedStatus("0");
+        driver.setStationId(stationId);//站点编号
+
+
+        driver.setSysDriverId(UUIDGenerator.getUUID());
+        driver.setPayCode(Encoder.MD5Encode(payCode.getBytes()));
+
+        try {
+            result = driverService.saveDriver(driver,operation);
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+        return "redirect:/web/driver/list/page";
+    }
 
 }
