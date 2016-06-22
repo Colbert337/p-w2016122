8eb4d7f9a4fdc92d91f33165aba5ef019033e7c6
wdq <dongqiang.wang@126.com>
2016-06-22 16:58:42 +0800

    添加车辆与车队关系基础代码

---
+++ src/main/java/com/sysongy/tcms/advance/model/TcFleetVehicle.java
@@ -0,0 +1,55 @@
+package com.sysongy.tcms.advance.model;
+
+import java.util.Date;
+
+public class TcFleetVehicle {
+    private String tcFleetVehicleId;
+
+    private String stationId;
+
+    private String tcFleetId;
+
+    private String tcVehicleId;
+
+    private Date createdDate;
+
+    public String getTcFleetVehicleId() {
+        return tcFleetVehicleId;
+    }
+
+    public void setTcFleetVehicleId(String tcFleetVehicleId) {
+        this.tcFleetVehicleId = tcFleetVehicleId == null ? null : tcFleetVehicleId.trim();
+    }
+
+    public String getStationId() {
+        return stationId;
+    }
+
+    public void setStationId(String stationId) {
+        this.stationId = stationId == null ? null : stationId.trim();
+    }
+
+    public String getTcFleetId() {
+        return tcFleetId;
+    }
+
+    public void setTcFleetId(String tcFleetId) {
+        this.tcFleetId = tcFleetId == null ? null : tcFleetId.trim();
+    }
+
+    public String getTcVehicleId() {
+        return tcVehicleId;
+    }
+
+    public void setTcVehicleId(String tcVehicleId) {
+        this.tcVehicleId = tcVehicleId == null ? null : tcVehicleId.trim();
+    }
+
+    public Date getCreatedDate() {
+        return createdDate;
+    }
+
+    public void setCreatedDate(Date createdDate) {
+        this.createdDate = createdDate;
+    }
+}
