9bb8ed055171ca78361ef32288ad76402c10ead7
wdq <dongqiang.wang@126.com>
2016-07-25 10:27:27 +0800

    添加CRM内容管理基础代码

---
+++ src/main/java/com/sysongy/crm/help/controller/CrmHelpController.java
@@ -0,0 +1,21 @@
+package com.sysongy.crm.help.controller;
+
+import com.sysongy.poms.base.controller.BaseContoller;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ * @FileName: CrmHelpController
+ * @Encoding: UTF-8
+ * @Package: com.sysongy.crm.help.controller
+ * @Link: http://www.sysongy.com
+ * @Created on: 2016年07月25日, 10:24
+ * @Author: dongqiang.wang [wdq_2012@126.com]
+ * @Version: V2.0 Copyright(c)陕西司集能源科技有限公司
+ * @Description:
+ */
+@Controller
+@RequestMapping("/web/crm/")
+public class CrmHelpController extends BaseContoller{
+
+}
---
+++ src/main/java/com/sysongy/crm/help/service/CrmHelpService.java
@@ -0,0 +1,14 @@
+package com.sysongy.crm.help.service;
+
+/**
+ * @FileName: CrmHelpService
+ * @Encoding: UTF-8
+ * @Package: com.sysongy.crm.help.service
+ * @Link: http://www.sysongy.com
+ * @Created on: 2016年07月25日, 10:24
+ * @Author: dongqiang.wang [wdq_2012@126.com]
+ * @Version: V2.0 Copyright(c)陕西司集能源科技有限公司
+ * @Description:
+ */
+public interface CrmHelpService {
+}
