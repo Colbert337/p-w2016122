2dfd3d27ca5ea9977f74d172ef66a2dcfe5d230d
Henry <kdash198543@163.com>
2016-08-10 16:00:43 +0800

    no message

--- src/main/java/com/sysongy/api/mobile/tools/returncash/ReturnCashUtil.java
+++ src/main/java/com/sysongy/api/mobile/tools/returncash/ReturnCashUtil.java
@@ -1,7 +1,29 @@
 package com.sysongy.api.mobile.tools.returncash;
 
+import com.alibaba.fastjson.JSON;
+import com.sysongy.api.mobile.model.base.MobileParams;
+import com.sysongy.api.mobile.model.base.MobileReturn;
+import com.sysongy.api.mobile.model.verification.MobileVerification;
 import com.sysongy.api.mobile.tools.MobileUtils;
+import com.sysongy.api.mobile.tools.login.MobileLoginUtils;
 
 public class ReturnCashUtil extends MobileUtils {
-
+	
+public static void checkParam(MobileParams param, MobileReturn ret) throws Exception{
+		
+//		try {
+//			verification = (MobileVerification) JSON.parseObject(param.getDetailParam(), MobileVerification.class);
+//		} catch (Exception e) {
+//			ret = MobileUtils.packagingMobileReturn(MobileLoginUtils.RET_ERROR, MobileLoginUtils.RET_PARAM_ERROR_MSG, null);
+//			throw e;
+//		}
+//
+//		checkApiKey(param.getApiKey(), ret);
+//		
+//		if (param == null || param.getDetailParam().length() != 11) {
+//			ret.setError(RET_ERROR);
+//			ret.setMsg(RET_PARAM_ERROR_MSG);
+//			throw new Exception(RET_PARAM_ERROR_MSG);
+//		}
+	}
 }
