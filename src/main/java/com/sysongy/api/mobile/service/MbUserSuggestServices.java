bb44656f6e518e947872b46c3d413aa41f0f74b4
zhangyaotian <yaotian.zhang@sysongy.com>
2016-08-12 14:59:22 +0800

    Revert "自己做了一半的错误代码"
    This reverts commit efacdf9e033aff74618b691e8fb234cebbffede1.

--- src/main/java/com/sysongy/api/mobile/service/MbUserSuggestServices.java
+++ src/main/java/com/sysongy/api/mobile/service/MbUserSuggestServices.java
@@ -1,12 +1,8 @@
 package com.sysongy.api.mobile.service;
 
-import java.util.List;
-
 import com.sysongy.api.mobile.model.feedback.MobileFeedBack;
-import com.sysongy.poms.suggest.model.Suggest;
 
 public interface MbUserSuggestServices {
 	
 	public Integer saveSuggester(MobileFeedBack obj) throws Exception;
-	public List<Suggest> queryForPage(Suggest suggest);
 }
