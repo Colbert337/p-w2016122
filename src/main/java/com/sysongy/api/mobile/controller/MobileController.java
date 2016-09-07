e2af02174c63658ed1b40b8a05733835f31c9563
wdq <dongqiang.wang@126.com>
2016-09-07 10:45:12 +0800

    修改图片路径

--- src/main/java/com/sysongy/api/mobile/controller/MobileController.java
+++ src/main/java/com/sysongy/api/mobile/controller/MobileController.java
@@ -1416,7 +1416,7 @@ public class MobileController {
 						bannerMap.put("title",banner.getTitle());
 						bannerMap.put("content","");
 						bannerMap.put("time",sft.format(banner.getCreatedDate()) );
-						bannerMap.put("contentUrl",banner.getTargetUrl());
+						bannerMap.put("contentUrl",http_poms_path+banner.getTargetUrl());
 						if(banner.getImgPath() != null && !"".equals(banner.getImgPath().toString())){
 							bannerMap.put("imageUrl",http_poms_path+banner.getImgPath());
 						}else{
