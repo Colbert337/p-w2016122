cb74c5d7f740bf452e31192dff3d73b5a5c9ad76
wdq <dongqiang.wang@126.com>
2016-09-08 15:49:57 +0800

    优化实名认证

--- src/main/java/com/sysongy/api/mobile/controller/MobileController.java
+++ src/main/java/com/sysongy/api/mobile/controller/MobileController.java
@@ -369,8 +369,8 @@ public class MobileController {
 						}
 
 						//获取用户审核状态
-						String driverStstus = driver.getCheckedStatus();
 						driver = driverlist.get(0);
+						String driverStstus = driver.getCheckedStatus();
 						if("2".equals(driverStstus)){
 							resultMap.put("nick",driver.getFullName());
 						}else{
