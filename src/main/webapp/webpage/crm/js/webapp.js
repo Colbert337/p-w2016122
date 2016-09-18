1c04bc7e74f3ec48f7da06026a135b732d366c34
gaoxiukun <xiukun.gao@sysongy.com>
2016-09-18 17:52:30 +0800

    修复分享下载提示页面

--- src/main/webapp/webpage/crm/js/webapp.js
+++ src/main/webapp/webpage/crm/js/webapp.js
@@ -8,21 +8,23 @@ $(function(){
 		}
 	});
 
-	// 手机号码验证    
-	jQuery.validator.addMethod("isMobile", function(value, element) {    
-	  var length = value.length;    
-	  return this.optional(element) || (length == 11 && /^(((13[0-9]{1})|(15[0-9]{1})|(18[0-9]{1}))+\d{8})$/.test(value));    
-	}, "请正确填写您的手机号码。");
+	if($(".form-validator").length){
+		// 手机号码验证    
+		jQuery.validator.addMethod("isMobile", function(value, element) {    
+		  var length = value.length;    
+		  return this.optional(element) || (length == 11 && /^(((13[0-9]{1})|(15[0-9]{1})|(18[0-9]{1}))+\d{8})$/.test(value));    
+		}, "请正确填写您的手机号码。");
 
-	// 只能输入[0-9]数字
-	jQuery.validator.addMethod("isDigits", function(value, element) {       
-	     return this.optional(element) || /^\d+$/.test(value);       
-	}, "只能输入0-9数字"); 
+		// 只能输入[0-9]数字
+		jQuery.validator.addMethod("isDigits", function(value, element) {       
+		     return this.optional(element) || /^\d+$/.test(value);       
+		}, "只能输入0-9数字"); 
+	}
 
     //提交表单验证
     if($("#shareInviteCode").length){
 		$("#shareInviteCode").validate({
-			//debug: true, //调试模式取消submit的默认提交功能    
+			//debug: true, //调试模式取消submit的默认提交功能
 			submitHandler: function(form) { //表单提交句柄,为一回调函数，带一个参数：form   
 				alert("提交表单");
 				form.submit(); //提交表单   
@@ -87,10 +89,14 @@ $(function(){
 	}
 
 	$('.logic-download-app').on('click',function(){
-		var $help = $('.download-app-help');
-		$help.show();
+		var html = '';
+		html += '<div class="download-app-help">'
+		html += '<img class="img" src="images/download-app-help.png" alt="">'
+		html += '<div class="transparent"></div>'
+		html += '</div>'
+		$('body').append(html);
 		setTimeout(function(){
-			$help.hide();
+			$('.download-app-help').remove();
 		},3000)
 	});
 });
