0e64fafdfc7c8cb2e5f047400098a7b2b08fbef8
wdq <dongqiang.wang@126.com>
2016-09-22 18:41:05 +0800

    添加邀请码发送页面

---
+++ src/main/java/com/sysongy/poms/permi/dao/SysUserAccountStrMapper.java
@@ -0,0 +1,9 @@
+package com.sysongy.poms.permi.dao;
+
+import com.sysongy.poms.permi.model.SysUserAccountStr;
+
+public interface SysUserAccountStrMapper {
+    int insert(SysUserAccountStr record);
+
+    int insertSelective(SysUserAccountStr record);
+}
