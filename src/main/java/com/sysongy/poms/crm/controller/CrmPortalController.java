13c8be4e8e3df18e3bc17e82084401b4ebae96c7
wangzhao <wangzao@sysongy.com>
2016-10-10 18:41:13 +0800

    站点详情分享

--- src/main/java/com/sysongy/poms/crm/controller/CrmPortalController.java
+++ src/main/java/com/sysongy/poms/crm/controller/CrmPortalController.java
@@ -415,21 +415,21 @@ public class CrmPortalController {
         Gastation gastation = gastationService.queryGastationByPK(stationId);
         //获取当前气站价格列表
         String price = gastation.getLng_price();
-        price = price.replaceAll("，",",");
-        price = price.replaceAll("：",":");
         List<Map<String,Object>> priceArray = new ArrayList<>();
-        if(price.indexOf(":")!=-1 && price.indexOf("/")!=-1) {
-            String strArray[] = price.split(",");
-
-            for (int i = 0; i < strArray.length; i++) {
-                Map<String, Object> dataMap = new HashMap<>();
-                dataMap.put("priceName", strArray[i]);
-                priceArray.add(dataMap);
-            }
+        if(price != null && !"".equals(price)){
+	        price = price.replaceAll("，",",");
+	        price = price.replaceAll("：",":");
+	        if(price.indexOf(":")!=-1 && price.indexOf("/")!=-1) {
+	            String strArray[] = price.split(",");
+	            for (int i = 0; i < strArray.length; i++) {
+	                Map<String, Object> dataMap = new HashMap<>();
+	                dataMap.put("priceName", strArray[i]);
+	                priceArray.add(dataMap);
+	            }
+	        }
         }
         map.addAttribute("gastation",gastation);
         map.addAttribute("priceList",priceArray);
-
         //统计分享数
         String viewCount = gastation.getViewCount();
         viewCount = String.valueOf(Integer.parseInt(viewCount)+1);
