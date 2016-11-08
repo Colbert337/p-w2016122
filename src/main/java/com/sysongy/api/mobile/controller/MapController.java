93461af802d913b28257dc4b83b1a4dc3b6abb4d
wdq <dongqiang.wang@126.com>
2016-11-08 17:44:27 +0800

    修改app邀请用户验证码验证失败问题

--- src/main/java/com/sysongy/api/mobile/controller/MapController.java
+++ src/main/java/com/sysongy/api/mobile/controller/MapController.java
@@ -3,7 +3,9 @@ package com.sysongy.api.mobile.controller;
 import com.sysongy.api.mobile.model.base.MobileReturn;
 import com.sysongy.api.util.DESUtil;
 import com.sysongy.poms.driver.model.SysDriver;
+import com.sysongy.util.HttpUtil;
 import com.sysongy.util.JsonTool;
+import net.sf.json.JSON;
 import net.sf.json.JSONObject;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -27,7 +29,8 @@ public class MapController {
     protected Logger logger = LoggerFactory.getLogger(this.getClass());
     public final String keyStr = "sysongys";
 
-    String routeAPI = "http://restapi.amap.com/v3/direction/driving?";
+    public static String  routeAPI = "http://restapi.amap.com/v3/direction/driving?";
+    public static String key = "849930967f293d450246fe6bc906a683";
 
     /**
      * 获取路径规划方案
@@ -43,7 +46,7 @@ public class MapController {
         result.setMsg("获取规划方案！");
         JSONObject resutObj = new JSONObject();
         String resultStr = "";
-        String key = "849930967f293d450246fe6bc906a683";
+
         try {
             /**
              * 解析参数
@@ -54,34 +57,30 @@ public class MapController {
             /**
              * 必填参数
              */
-            String token = "token";
-            String password = "password";
-            String verificationCode = "verificationCode";
-            boolean b = JsonTool.checkJson(mainObj, token, password, verificationCode);
+            String origin = "origin";
+            String destination = "destination";
+            String strategy = "strategy";
+            String output = "json";
+            boolean b = JsonTool.checkJson(mainObj, origin, destination, strategy);
             /**
              * 请求接口
              */
             if (b) {
-                SysDriver sysDriver = new SysDriver();
-                sysDriver.setSysDriverId(mainObj.optString("token"));
-                password = mainObj.optString("password");
-                if (password != null && !"".equals(password)) {
-                    sysDriver.setPassword(password);
-                    sysDriver.setSysDriverId(mainObj.optString("token"));
+                origin = mainObj.optString("origin");
+                destination = mainObj.optString("destination");
+                strategy = mainObj.optString("strategy");
+                String paramStr = "origin="+origin+"&destination="+destination+"&strategy="+strategy+"&key="+key;
 
-                } else {
-                    result.setStatus(MobileReturn.STATUS_FAIL);
-                    result.setMsg("密码为空！");
-                }
+                String mapResult = HttpUtil.sendPostUrl(routeAPI,paramStr,"UTF-8");
+                resultStr = mapResult;
+                result.setStatus(MobileReturn.STATUS_SUCCESS);
+                result.setMsg("获取规划方案成功！");
             } else {
                 result.setStatus(MobileReturn.STATUS_FAIL);
                 result.setMsg("参数有误！");
             }
-            resutObj = JSONObject.fromObject(result);
-            resutObj.remove("listMap");
-            resultStr = resutObj.toString();
+
             logger.error("修改登录密码信息： " + resultStr);
-            resultStr = DESUtil.encode(keyStr, resultStr);// 参数解密
         } catch (Exception e) {
             result.setStatus(MobileReturn.STATUS_FAIL);
             result.setMsg("修改登录密码失败！");
@@ -94,5 +93,35 @@ public class MapController {
         } finally {
             return resultStr;
         }
+    }
+
+    /**
+     * 获取指定位置的路径规划方案
+     * @param origin 出发点经纬度
+     * @param destination 目的地经纬度
+     * @param strategy 驾车选择策略 0~9
+     * @return
+     */
+    public static String getDirectionMap(String origin, String destination, String strategy){
+        JSONObject resultObj = new JSONObject();
+        try {
+            //获取路线规划结果
+            String returnStr = "";
+            String paramStr = "origin="+origin+"&destination="+destination+"&strategy="+strategy+"&key="+key;
+            String mapResult = HttpUtil.sendPostUrl(routeAPI,paramStr,"UTF-8");
+
+            resultObj = JSONObject.fromObject(mapResult);
+
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+
+
+        return null;
+    }
+
+    public static void main(String[] args) {
+        String resultStr = getDirectionMap("116.481028,39.989643","116.465302,40.004717","0");
+        System.out.println("resultStr:"+resultStr);
     }
 }
