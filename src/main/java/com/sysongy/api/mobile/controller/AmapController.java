a227a4b60bbd6fdfe1ceda34ff90b0b57c10e252
wdq <dongqiang.wang@126.com>
2016-11-08 12:01:28 +0800

    添加高德地图Controller

---
+++ src/main/java/com/sysongy/api/mobile/controller/AmapController.java
@@ -0,0 +1,14 @@
+package com.sysongy.api.mobile.controller;
+
+/**
+ * @FileName: AmapController
+ * @Encoding: UTF-8
+ * @Package: com.sysongy.api.mobile.controller
+ * @Link: http://www.sysongy.com
+ * @Created on: 2016年11月08日, 12:00
+ * @Author: dongqiang.wang [wdq_2012@126.com]
+ * @Version: V2.0 Copyright(c)陕西司集能源科技有限公司
+ * @Description:
+ */
+public class AmapController {
+}
