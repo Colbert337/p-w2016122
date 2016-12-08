bfb282e2e8056b6df4888273acdbae8d15f4dd9f
wdq <dongqiang.wang@126.com>
2016-12-08 18:49:51 +0800

    修改CRM冲红失败，冲红成功时消费报表不显示冲红订单问题

--- src/main/java/com/sysongy/poms/order/service/impl/OrderServiceImpl.java
+++ src/main/java/com/sysongy/poms/order/service/impl/OrderServiceImpl.java
@@ -135,6 +135,8 @@ public class OrderServiceImpl implements OrderService {
 				}
 			}
 		}
+		//设置冲红订单状态为成功
+		record.setOrderStatus(GlobalConstant.ORDER_STATUS.ORDER_SUCCESS);
 		return sysOrderMapper.insert(record);
 	}
 
